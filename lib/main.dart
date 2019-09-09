3cc2ff0f234b4a21858caf58f7d15ef3247df579
ElmerYam <elmeryam5@gmail.com>
2019-09-09 11:40:50 -0500

    consumiendo api de las oficcinas de google

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+//uso de las ubicaciones de la api
+import 'src/locations.dart' as locations;
 
 void main() => runApp(MyApp());
 
@@ -9,9 +11,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Google Office Locations'),
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: const LatLng(0, 0),
+              zoom: 2,
+            ),
+            markers: _markers.values.toSet(),
+          ),
+        ),
+      );
+}
+
+/*class _MyAppState extends State<MyApp> {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _chetumal = const LatLng(18.519281, -88.301937);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -28,11 +69,11 @@ class _MyAppState extends State<MyApp> {
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: _chetumal,
+            zoom: 13.0,
           ),
         ),
       ),
     );
   }
-}
+}*/
